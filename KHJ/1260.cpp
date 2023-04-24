dd1706d23976388297c0c1fad0180d9f5c7458f1
Kwun Hyeong Jin <neo4130@gmail.com>
2023-04-24 23:35:56 +0900

    Update 1260.cpp
    이제보니 BFS에서 초기 부분을 두번 출력한것 같네요

--- KHJ/1260.cpp
+++ KHJ/1260.cpp
@@ -2,17 +2,15 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_VALUE = 10001;
-int edge[MAX_VALUE][MAX_VALUE]; //  인접한 노드
-bool visit[MAX_VALUE]; //   방문 여부
+bool path[1001][1001], visit[1001]; //  인접한 노드, 방문 여부
 queue<int> q;
 int nodeCount, wayCount, startNode; //  정점 개수, 간선 개수, 시작 정점
 
 void ResetVisit() //    방문 초기화
 {
-    for(int ix = 1; ix <= nodeCount; ix++)
+    for(int ix = 0; ix <= nodeCount; ix++)
     {
-        visit[ix] = 0;
+        visit[ix] = false;
     }
 }
 
@@ -23,7 +21,7 @@ void DFS(int startNode) //  깊이 탐색
 
     for(int ix = 1; ix <= nodeCount; ix++)
     {
-        if(edge[startNode][ix] == 1 && visit[ix] == 0)
+        if(startNode != ix && path[startNode][ix]&& !visit[ix])
         {
             DFS(ix);
         }
@@ -34,19 +32,19 @@ void BFS(int startNode) // 넓이 탐색
 {
     q.push(startNode);
     visit[startNode] = true;
-    cout << startNode << " ";
-    
+
     while (!q.empty())
     {
-        startNode = q.front();
+        int nextNode = q.front();
         q.pop();
-
+        cout << nextNode << " ";
         for(int ix = 1; ix <= nodeCount; ix++)
         {
-            if(visit[ix]) continue;
-            q.push(ix);
-            visit[ix] = true;
-            cout << ix << " ";
+            if(!visit[ix] && nextNode != ix && path[nextNode][ix])
+            {
+                q.push(ix);
+                visit[ix] = true;
+            }
         }
     }
 }
@@ -58,16 +56,14 @@ int main()
     {
         int a, b;
         cin >> a >> b; // 입력으로 주어진 간선은 양방향이니 양쪽 다 등록한다.
-        edge[a][b] = 1;
-        edge[b][a] = 1;
+        path[a][b] = path[b][a] = true;
     }
     ResetVisit(); // 방문 노드 초기화
     DFS(startNode);
-
     cout << '\n';
-
     ResetVisit();
     BFS(startNode);
+    cout << '\n';
     
     return 0;
 }
