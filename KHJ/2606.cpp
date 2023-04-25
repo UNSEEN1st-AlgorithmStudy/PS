1603516eb59745ce80bfc77358019e0082aa6d78
Kwun Hyeong Jin <neo4130@gmail.com>
2023-04-25 21:17:03 +0900

    2606 풀이 수정
    검색 제한을 경로수가아니라 총 카운트로 해야하는데 실수했군요

--- KHJ/2606.cpp
+++ KHJ/2606.cpp
@@ -2,19 +2,19 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-const int MAX_PC = 100, MAX_PATH = 50;
+const int MAX_PC = 101, MAX_PATH = 101;
 bool wormed[MAX_PC], path[MAX_PATH][MAX_PATH]; // 바이러스에 걸린 컴퓨터, 확인된 경로
-int pcCount, connectCount, totalWormedPC;             
+int pcCount, connectCount, totalWormedPC;
 
-void DFS(int node){
+void DFS(int node) {
     // 해당 노드에 대한 방문 처리
     wormed[node] = true;
+    if (node != 1)totalWormedPC++;
     // 이곳과 연결된 노드들 중 방문하지 않은 노드 탐색하기
-    for(int i = 1; i <= connectCount; i++)
+    for (int i = 1; i <= pcCount; i++)
     {
-        if(!wormed[i] && path[node][i])
+        if (!wormed[i] && path[node][i])
         {
-            totalWormedPC++;
             DFS(i);   // 재귀 호출로 구현
         }
     }
@@ -24,17 +24,17 @@ int main()
 {
     memset(wormed, false, sizeof(wormed)); // false로 초기화
     memset(path, false, sizeof(path));     // false로 초기화
-    
+
     cin >> pcCount; // 컴퓨터의 수
     cin >> connectCount; // 네트워크 상에서 연결된 쌍의 수
-    for(int i = 1; i <= connectCount; i++)
+    for (int i = 0; i < connectCount; i++)
     {
-        int a,b;
+        int a, b;
         cin >> a >> b;
         path[a][b] = path[b][a] = true;
     }
 
     DFS(1);
-    cout << totalWormedPC << '\n';
+    cout << totalWormedPC;
     return 0;
 }
