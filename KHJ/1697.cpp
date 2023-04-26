6203b6dbbf513eafb53205cf571eda9afff3cb86
Kwun Hyeong Jin <neo4130@gmail.com>
2023-04-27 00:36:49 +0900

    1697 풀이
    이번에는 좀 많이 어려웠네요.. 여러 답안을 보기도 했는데 이해가 어려운 답안도 많고 다소 난이도 있었던 문제였습니다

---
+++ KHJ/1697.cpp
@@ -0,0 +1,50 @@
+﻿#include "iostream"
+#include "queue"
+using namespace std;
+const int MAX_NODE = 100001;
+bool visit[MAX_NODE];
+
+int SearchBFS(int N, int K)
+{
+    queue<int> q;
+    visit[N] = true;
+    q.push(N);
+    int cnt = 0;
+    while (!q.empty())
+    {
+        int qSize = q.size();
+        for(int i = 0; i < qSize; i++)
+        {
+            int cur = q.front();
+            q.pop();
+            if(cur == K)
+            {
+                return cnt;
+            }
+            if(cur+1 <= MAX_NODE && !visit[cur+1])
+            {
+                q.push(cur + 1);
+                visit[cur+1] = true;
+            }
+            if(cur-1 >= 0 && !visit[cur -1])
+            {
+                q.push(cur - 1);
+                visit[cur-1] = true;
+            }
+            if(cur*2 <= MAX_NODE && !visit[cur*2])
+            {
+                q.push(cur*2);
+                visit[cur*2] = true;
+            }
+        }
+        cnt++;
+    }
+}
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    cout << SearchBFS(N,K);
+    return 0;
+}
