db341098ea5b03ceea5cce451ca152d547a534fc
Kwun Hyeong Jin <neo4130@gmail.com>
2023-04-30 07:46:46 +0900

    2667 못풀이
    이거이거 저를 고통받게 하는 문제입니다.
    뭔가 자력으로 풀 수 있을거 같아서 머리뽀개지게 만들었는데 백준에서는 자꾸 틀렸다하네요...
    일단 구동은 되어서 코드 올려봅니다.

---
+++ KHJ/2667.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+const int MAP_SIZE = 25;
+vector<int> totalComplex; 
+
+int dx[] = { 1, 0, -1, 0};
+int dy[] = { 0, 1, 0, -1};
+
+int M, currentComplex = 0;
+
+bool visit[MAP_SIZE][MAP_SIZE] = {0,};
+int graph[MAP_SIZE][MAP_SIZE];
+
+bool IsValid(int ny, int nx)
+{
+    return (nx >= 0 && ny >= 0 && nx < M && ny < M);
+}
+
+void DFS(int y, int x)
+{
+    visit[y][x] = true;
+    totalComplex.at(currentComplex)++;
+    for(int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(IsValid(ny,nx) && !visit[ny][nx] &&graph[ny][nx] == 1)
+        {
+            DFS(ny, nx);
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d", &M);
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+    
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            if(graph[i][j] != 1 || visit[i][j])
+            {
+                continue;
+            }
+            totalComplex.push_back(0);
+            DFS(i,j);
+            currentComplex++;
+        }
+    }
+    sort(totalComplex.begin(), totalComplex.end());
+    printf("%d\n",totalComplex.size());
+    for(auto idx : totalComplex) printf("%d\n", idx);
+    return 0;
+}
