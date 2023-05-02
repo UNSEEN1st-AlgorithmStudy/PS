256707a0aa69b990256801c02ad064696f243ce3
Kwun Hyeong Jin <neo4130@gmail.com>
2023-05-02 19:26:45 +0900

    10026 풀이
    이번에도 무난하게 풀긴 했습니다.
    다만 아쉬웠던건 BFS를 두번 돌리게 되었는데, 한 번만에 다 돌릴 수 있을 것 같은데~~ 막상 구상이 안떠오르네요...

---
+++ KHJ/10026.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+const int MAP_SIZE = 101;
+
+struct color
+{
+    int y, x;
+};
+
+int dx[] = { 1, 0, -1, 0};
+int dy[] = { 0, 1, 0, -1};
+
+int N, totalArea_0, totalArea_1 = 0;
+
+bool visit[MAP_SIZE][MAP_SIZE];
+char graph[MAP_SIZE][MAP_SIZE];
+
+queue<color> q;
+
+bool IsValid(int ny, int nx)
+{
+    return (nx >= 0 && ny >= 0 && nx < N && ny < N);
+}
+
+void BFS(int a, int b)
+{
+    q.push({a,b});
+    visit[a][b] = true;
+
+    while (!q.empty())
+    {
+        int y = q.front().y;
+        int x = q.front().x;
+        q.pop();
+
+        for(int i = 0; i < 4; i++)
+        {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(IsValid(ny,nx) && !visit[ny][nx] && graph[ny][nx] == graph[y][x])
+            {
+                visit[ny][nx] = true;
+                q.push({ny,nx});
+            }
+        }
+    }
+}
+
+int main()
+{
+    //  입력
+    cin >> N;
+    for(int i=0; i < N; i++)
+    {
+        for(int j=0; j < N; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            if(!visit[i][j])
+            {
+                BFS(i,j);
+                totalArea_0++;
+            }
+        }
+    }
+
+    memset(visit, false, sizeof(visit));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (graph[i][j] == 'G') graph[i][j] = 'R';
+        }
+    }
+ 
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (!visit[i][j])
+            {
+                BFS(i, j);
+                totalArea_1++;
+            }
+        }
+    }
+    cout << totalArea_0 << " " << totalArea_1 << '\n';
+    return 0;
+}
