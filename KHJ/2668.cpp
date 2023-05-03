7f8767badf8981b761d4f38cf41e73c040eef34b
Kwun Hyeong Jin <neo4130@gmail.com>
2023-05-03 17:41:16 +0900

    2668 풀이
    이번에도 또 새로운 그래프 형식의 문제라 초반에 많이 헷갈렸네요
    딱 고민하던 부분에서 Set 자료 구조를 활용한 방법도 있어서 많은 참고가 되었습니다.

---
+++ KHJ/2668.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+const int ROW_SIZE = 101;
+int N = 0, G = 0;
+int row[ROW_SIZE];
+int result[ROW_SIZE];
+bool visit[ROW_SIZE] = {false};
+
+void DFS(int cur, int start)
+{
+    if(visit[cur])
+    {
+        if(cur == start) result[G++] = start; // 결과 배열에는 작은 수부터 입력 된다. 
+    }
+    else
+    {
+        visit[cur] = true;
+        DFS(row[cur], start);
+    }
+}
+
+int main()
+{
+    cin >> N; // 최대 정수 입력
+    for(int i = 1; i <= N; i++) // 배열에 주어진 번호 입력
+    {
+        cin >> row[i];
+    }
+    
+    for(int i = 1; i <= N; i++) // 모든 인덱스 DFS 탐색
+    {
+        memset(visit, false, sizeof(visit));
+        DFS(i, i);
+    }
+    
+    cout << G << '\n';
+    
+    for(int i = 0; i < G; i++)
+    {
+        cout << result[i] << '\n';
+    }
+    
+    return 0;
+}
