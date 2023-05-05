9c52e0fdd43c6a6a97ad53e922d9e1cb90142222
Kwun Hyeong Jin <neo4130@gmail.com>
2023-05-05 09:00:31 +0900

    13023 풀이
    처음 봤을 때는 입력 값에 따라 문제가 발생하면 입력 값을 정렬해주면 괜찮지 않을까? 라고 생각했는데 알고리즘이란게 정렬 가능한 값들만 주어지는 경우만 사용하는게 아니니 매우 흥미로웠네요.
    설명까지 같이 들었으면 좋았을텐데 제가 몸 상태가 안 좋았어서 아쉽습니다.

---
+++ KHJ/13023.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+const int MAX_COUNT = 2001;
+vector<int> friendship[MAX_COUNT];
+bool visit[MAX_COUNT];
+int answer = 0;
+int N = 0, M = 0;
+
+void DFS(int cur, int friendCount)
+{
+    visit[cur] = true;
+    if(friendCount == 4)
+    {
+        answer = 1;
+        return;
+    }
+    for(auto nextFriend : friendship[cur])
+    {
+        if(visit[nextFriend]) continue;
+        DFS(nextFriend, friendCount + 1);
+    }
+    visit[cur] = false;
+}
+
+int main()
+{
+    cin >> N >> M;
+    for(int i = 0; i < M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        friendship[a].push_back(b);
+        friendship[b].push_back(a);
+    }
+    for(int i = 0; i < N; i++)
+    {
+        DFS(i, 0);
+    }
+    cout << answer << '\n';
+    return 0;
+}
