fb09eafab18414ff6ba4cd005ffa2dcb9566adc0
Kwun Hyeong Jin <neo4130@gmail.com>
2023-05-09 09:55:24 +0900

    14500 풀이 실패
    요즘 프로젝트 진행하느라 바쁘다 보니 너무 미루어지는 것 같아서 알고리즘 간만에 잡았습니다.
    일단 주어진 예제에 대한 부분들은 전부 통과가 되었는데 어째선지 실패하는군요..
    이전에 설명해주신게 기억나서 ㅗ 모양만 돌아가서 체크해주는 별도의 함수 작성해주고 나머지는 회전이나 대칭이 이루어진다 하면 이어지는 깊이 값 체크라 되어야할텐데 왜 안되는지 모르겠군요

---
+++ KHJ/14500.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+const int MAX_SIZE = 500;
+int dx[] = { 1, 0, -1, 0};
+int dy[] = { 0, 1, 0, -1};
+int N, M, answer;
+int graph[MAX_SIZE][MAX_SIZE];
+bool visit[MAX_SIZE][MAX_SIZE];
+
+bool IsInside(int ny, int nx)
+{
+    return (nx >= 0 && ny >= 0 && nx < M && ny < N);
+}
+
+void DFS(int a, int b, int depth, int sum)
+{
+    if(depth == 3)
+    {
+        answer = max(answer, sum);
+        return;
+    }
+    for(int i = 0; i < 4; i++)
+    {
+        int ny = a + dy[i];
+        int nx = b + dx[i];
+        if(!IsInside(ny, nx)) continue;
+        if(visit[ny][nx])continue;
+        visit[ny][nx] = true;
+        DFS(ny, nx, depth+1, sum+graph[ny][nx]);
+        visit[ny][nx] = false;
+    }
+}
+
+void ArrowShape(int a, int b)
+{
+    int sum = 0;
+    sum = graph[a][b] + graph[a - 1][b + 1] + graph[a][b + 1] + graph[a + 1][b + 1];
+    answer = max(answer, sum);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    answer = 0;
+    cin >> N >> M;
+    for( int i = 0; i < N; i++) // Input
+    {
+        for(int j = 0; j < M; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            visit[i][j] = true;
+            DFS(i, j, 0, graph[i][j]);
+            visit[i][j] = false;
+            if(i + 1 < N && i - 1 >= 0 && j + 1 < M) ArrowShape(i, j); // If ArrowShape Is Answer
+        }
+    }
+
+    cout << answer;
+}
