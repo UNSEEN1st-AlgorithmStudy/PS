d7c2f8427035ea85759f1d9d187aba29a58d2b71
Kwun Hyeong Jin <neo4130@gmail.com>
2023-05-09 14:29:57 +0900

    14500 풀이
    ㅗ 도형 하나만 함수를 작성했었네요 나머지 ㅓ ㅏ ㅜ 방향에 대한 함수 작성이 없었던거였습니다

--- KHJ/14500.cpp
+++ KHJ/14500.cpp
@@ -4,8 +4,8 @@
 #include <cstring>
 using namespace std;
 const int MAX_SIZE = 500;
-int dx[] = { 1, 0, -1, 0};
-int dy[] = { 0, 1, 0, -1};
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
 int N, M, answer;
 int graph[MAX_SIZE][MAX_SIZE];
 bool visit[MAX_SIZE][MAX_SIZE];
@@ -17,24 +17,45 @@ bool IsInside(int ny, int nx)
 
 void DFS(int a, int b, int depth, int sum)
 {
-    if(depth == 3)
+    if (depth == 3)
     {
         answer = max(answer, sum);
         return;
     }
-    for(int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
         int ny = a + dy[i];
         int nx = b + dx[i];
-        if(!IsInside(ny, nx)) continue;
-        if(visit[ny][nx])continue;
+        if (!IsInside(ny, nx)) continue;
+        if (visit[ny][nx])continue;
         visit[ny][nx] = true;
-        DFS(ny, nx, depth+1, sum+graph[ny][nx]);
+        DFS(ny, nx, depth + 1, sum + graph[ny][nx]);
         visit[ny][nx] = false;
     }
 }
 
-void ArrowShape(int a, int b)
+void ArrowShape1(int a, int b)
+{
+    int sum = 0;
+    sum = graph[a][b] + graph[a][b + 1] + graph[a][b + 2] + graph[a - 1][b + 1];
+    answer = max(answer, sum);
+}
+
+void ArrowShape2(int a, int b)
+{
+    int sum = 0;
+    sum = graph[a][b] + graph[a][b + 1] + graph[a][b + 2] + graph[a + 1][b + 1];
+    answer = max(answer, sum);
+}
+
+void ArrowShape3(int a, int b)
+{
+    int sum = 0;
+    sum = graph[a][b] + graph[a + 1][b] + graph[a + 2][b] + graph[a + 1][b + 1];
+    answer = max(answer, sum);
+}
+
+void ArrowShape4(int a, int b)
 {
     int sum = 0;
     sum = graph[a][b] + graph[a - 1][b + 1] + graph[a][b + 1] + graph[a + 1][b + 1];
@@ -48,24 +69,27 @@ int main()
     cout.tie(NULL);
     answer = 0;
     cin >> N >> M;
-    for( int i = 0; i < N; i++) // Input
+    for (int i = 0; i < N; i++)
     {
-        for(int j = 0; j < M; j++)
+        for (int j = 0; j < M; j++)
         {
             cin >> graph[i][j];
         }
     }
 
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        for(int j = 0; j < M; j++)
+        for (int j = 0; j < M; j++)
         {
             visit[i][j] = true;
             DFS(i, j, 0, graph[i][j]);
             visit[i][j] = false;
-            if(i + 1 < N && i - 1 >= 0 && j + 1 < M) ArrowShape(i, j); // If ArrowShape Is Answer
+            if (i - 1 >= 0 && j + 2 < M) ArrowShape1(i, j);
+            if (j + 2 < M && i + 1 < N) ArrowShape2(i, j);
+            if (i + 2 < N && j + 1 < M) ArrowShape3(i, j);
+            if (i + 1 < N && i - 1 >= 0 && j + 1 < M) ArrowShape4(i, j);
         }
     }
 
-    cout << answer;
+    cout << answer << '\n';
 }
