402cc1d9db21313bc9859534ba29fb03f7f8129c
Kwun Hyeong Jin <neo4130@gmail.com>
2023-06-02 04:08:23 +0900

    [KHJ] 1149 풀이
    음 조금씩 이제 감이 잡히는 것 같은데 여전히 어렵네요.
    이 비용이 쌓인다는 것을 잘 생각하는게 중요할 것 같습니다.
    여기서 느낀점은 다음 비용을 고려해서 진행하는게 아니라 이전의 비용을 생각해서 쌓아나가는 방식이 알맞는 것 같네요.
    이전 리뷰에 따라 인덱싱도 0으로 한번 바꾸어봤습니다.

---
+++ KHJ/1149.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+const int MAX = 1001; 
+int DP[MAX][3];
+int cost[3]; // 비용. 0 빨간색, 1 초록색, 2 파란색 이다.
+int N;
+
+// 이전 집 색깔중 최소 비용을 골라 현재 집 색깔을 선택하여 이어나간다. 
+
+int main()
+{
+    cin >> N; // 집의 수
+    DP[0][0] = 0;
+    DP[0][1] = 0;
+    DP[0][2] = 0;
+    
+    for(int i = 0; i < N; i++)
+    {
+        cin >> cost[0] >> cost[1] >> cost[2];
+        DP[i+1][0] = min(DP[i][1], DP[i][2]) + cost[0];
+        DP[i+1][1] = min(DP[i][0], DP[i][2]) + cost[1];
+        DP[i+1][2] = min(DP[i][1], DP[i][0]) + cost[2];
+    }
+    cout << min(DP[N][0],min(DP[N][1],DP[N][2]));
+}
