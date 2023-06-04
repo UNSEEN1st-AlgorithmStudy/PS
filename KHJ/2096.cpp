8252f1889b3a843c88fd3b679889f4f536e8c10e
Kwun Hyeong Jin <neo4130@gmail.com>
2023-06-04 13:43:21 +0900

    [KHJ] 2096 메모리 초과
    이 문제.. 범상치 않습니다. 보자마자 RGB 문제랑 유사하길래 '어.. 그러면 비슷하게 풀면 되겠다' 싶었는데, 최소 최대값을 구해야 합니다.
    그래서 pair 활용해서 좌측에는 최소값 우측에는 최대값을 저장하고 결과를 뽑아냈는데 역시나 메모리가 나가네요

---
+++ KHJ/2096.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+const int MAX = 100001;
+int score[3];
+int N;
+pair<int,int> DP[MAX][3];
+int main()
+{
+    cin >> N;
+
+    for(int i = 0; i < 3; i++)
+    {
+        DP[0][i] = {0,0};
+    }
+    
+    for(int i = 0; i < N; i++)
+    {
+        cin >> score[0] >> score[1] >> score[2];
+        DP[i+1][0] = {min(DP[i][0].first,DP[i][1].first ) + score[0], max(DP[i][0].second, DP[i][1].second) + score[0]};
+        DP[i+1][1] = {min(DP[i][0].first,min(DP[i][1].first,DP[i][2].first)) + score[1], max(DP[i][0].second, max(DP[i][1].second,DP[i][2].second)) + score[1]};
+        DP[i+1][2] = {min(DP[i][1].first,DP[i][2].first ) + score[2], max(DP[i][1].second, DP[i][2].second) + score[2]};
+    }
+    cout << max(DP[N][0].second, max(DP[N][1].second,DP[N][2].second)) << " " << min(DP[N][0].first,min(DP[N][1].first,DP[N][2].first)) << '\n';
+}
