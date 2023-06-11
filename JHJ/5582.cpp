cb1463cdb11957dd886569d657ed6612bd87dd38
Husk <68003176+HUSK-321@users.noreply.github.com>
2023-06-11 13:39:31 +0900

    5582 문제 풀이

---
+++ JHJ/5582.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 4001;
+int LCS[MAX][MAX];
+string s1, s2;
+
+int main(){
+    int maxLength = -1;
+
+    cin >> s1;
+    cin >> s2;
+
+    for(int i = 1; i <= s1.size(); i++){
+        for(int j = 1; j <= s2.size(); j++){
+            LCS[i][j] = (s1[i-1] == s2[j-1]) ? LCS[i-1][j-1] + 1 : 0;
+            maxLength = max(LCS[i][j], maxLength);
+        }
+    }
+
+    cout << maxLength << '\n';
+}
