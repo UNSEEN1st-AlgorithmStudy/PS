575116faf5379b0c0d3f5768fa8dd6ec68fbe5cb
Kwun Hyeong Jin <neo4130@gmail.com>
2023-06-13 19:16:58 +0900

    [KHJ] 15482 풀이
    아까 한준님 풀이보면서 wstring 이라고 하셨길래 저도 wstring 해보려고 했더니 런타임 에러가 뜨네요...
    결국 유니코드상 표기되는 바이트 크기대로 쪼개서 확인하는 방법밖에 없는 것 같습니다.

---
+++ KHJ/15482.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+const int MAX = 3003;
+int LCS[MAX][MAX];
+string s1, s2;
+
+int main()
+{
+    cin >> s1 >> s2;
+    for(int i = 3; i <= s1.size(); i+=3)
+    {
+        for(int j = 3; j <= s2.size(); j+=3)
+        {
+            LCS[i][j] = (s1.substr(i-3, 3) == s2.substr(j-3,3)) ? LCS[i][j] = LCS[i-3][j-3] + 1 : max(LCS[i-3][j], LCS[i][j-3]);
+        }
+    }
+
+    cout << LCS[s1.size()][s2.size()];
+    return 0;
+}
