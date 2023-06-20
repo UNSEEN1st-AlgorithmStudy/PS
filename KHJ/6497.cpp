e76eb7f33e9d0a41bdf82092807ef16892f4d8ac
Kwun Hyeong Jin <neo4130@gmail.com>
2023-06-20 10:33:17 +0900

    [KHJ] 6497 풀이
    이번 문제는 많이 헷갈렸네요..
    테스트 케이스가 여럿이란 부분을 인지를 못해서 while 문이 왜들어갔는지 의아했었네요.
    그밖에는 출력값을 제대로 확인 안해서 자꾸 최소비용만 넣었던 것 같습니다.

---
+++ KHJ/6497.cpp
@@ -0,0 +1,63 @@
+﻿#include <algorithm>
+#include <iostream>
+using namespace std;
+constexpr int MAX = 200012;
+pair<int, pair<int, int>> edge[MAX];
+int parents[MAX];
+int V, E, total, result = 0;
+
+void Init()
+{
+    total = result = 0;
+    for(int i = 0; i < MAX; i++) parents[i] = i;
+}
+
+int FindRoot(int x)
+{
+    if(x == parents[x]) return x;
+    return parents[x] = FindRoot(parents[x]);
+}
+
+void Merge(int x, int y)
+{
+    x = FindRoot(x);
+    y = FindRoot(y);
+
+    if(x == y) return;
+    parents[x] = y;
+}
+
+int main()
+{
+    while (true)
+    {
+        Init();
+        cin >> V >> E;
+        if(V == 0 && E == 0) break;
+        
+        for(int i = 0; i < E; i++)
+        {
+            int a,b,c;
+            cin >> a >> b >> c;
+            edge[i] = {c,{a,b}};
+            total += c;
+        }
+    
+        sort(edge, edge+E);
+        
+
+        for(int i = 0; i < E; i++)
+        {
+            if(FindRoot(edge[i].second.first) == FindRoot(edge[i].second.second))
+                continue;
+
+            Merge(edge[i].second.first, edge[i].second.second);
+            result += edge[i].first;
+        
+        }
+
+        cout << total - result << '\n';
+    }
+    
+    return 0;
+}
